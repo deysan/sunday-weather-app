11fbddfb9100560bbf4e1e3d2a1e99bb4224a2d0
deysan <ideysan@gmail.com>
2022-07-10 00:37:22 +0300

    Add location

--- src/components/weather-list/index.tsx
+++ src/components/weather-list/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from '@mui/material';
-import { useAppSelector } from 'hooks';
+import { useAppDispatch, useAppSelector } from 'hooks';
 import { WeatherCard } from 'components';
-import { selectAllCities } from 'store/cities';
+import { addCity, selectAllCities } from 'store/cities';
+import axios from 'axios';
 
 interface WeatherListProps {
   children: React.ReactNode;
 }
 
 export const WeatherList: React.FC<WeatherListProps> = ({ children }) => {
+  const dispatch = useAppDispatch();
   const cities = useAppSelector(selectAllCities);
+
+  const getGeolocation = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_IPGEOLOCATION_API_KEY}`,
+      );
+      dispatch(
+        addCity({
+          name: response.data.city,
+          fullName: `${response.data.city}, ${response.data.country_name}`,
+          lat: response.data.latitude,
+          lng: response.data.longitude,
+        }),
+      );
+    } catch (error) {
+      console.error('Something went wrong getting Geolocation from API!');
+    }
+  };
+
+  useEffect(() => {
+    if (!cities.length) getGeolocation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Grid container spacing={3}>
